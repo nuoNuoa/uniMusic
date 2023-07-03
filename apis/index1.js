f4a455c1433ee4060f54664f77263a0de3c73028
xinnuo <2028911220@qq.com>
2023-07-03 11:01:24 +0800

    create index page, api

---
+++ apis/index1.js
@@ -0,0 +1,104 @@
+const baseUrl = "https://www.gzamon.wang/api"
+// 获取轮播图
+export function apiGetBanner(data) {
+	return uni.request({
+		url: baseUrl + '/banner',
+		method: 'GET',
+		data,
+	})
+}
+// 获取推荐歌单
+export function apiGetRecommendSongs(data) {
+	return uni.request({
+		url: baseUrl + '/personalized',
+		method: 'GET',
+		data,
+	})
+}
+// 获取index
+// export function apiGetIndexData(data) {
+// 	return request.request({
+// 		url: '/aaIndex',
+// 		method: 'GET',
+// 		data,
+// 	})
+// }
+// 获取新碟
+export function apiGetTopAlbum(data) {
+	return uni.request({
+		url: baseUrl + '/album/newest',
+		method: 'GET',
+		data,
+	})
+}
+
+export function apiGetRelatedVideo(data) {
+	return uni.request({
+		url: baseUrl + '/comment/video',
+		method: 'GET',
+		data,
+		hideLoading: true
+	})
+}
+
+export function apiGetHotList(data) {
+	return uni.request({
+		url: baseUrl + '/top/playlist',
+		method: 'GET',
+		data,
+	})
+}
+
+// 获取歌单详情
+export function apiAlbumDetail(data) {
+	return uni.request({
+		url: baseUrl + '/playlist/detail',
+		method: 'GET',
+		data,
+	})
+}
+
+// 获取搜索
+export function apiSearch(data) {
+	return uni.request({
+		url: baseUrl + '/search',
+		method: 'GET',
+		data,
+	})
+}
+
+// 获取排行榜
+export function apiToplist(data) {
+	return uni.request({
+		url: baseUrl + '/toplist',
+		method: 'GET',
+		data,
+	})
+}
+
+// 获取排行榜
+export function apiTopSinglelist(data) {
+	return uni.request({
+		url: baseUrl + '/top/list',
+		method: 'GET',
+		data,
+	})
+}
+
+// 获取歌曲链接
+export function apiSongUrl(data) {
+	return uni.request({
+		url: baseUrl + '/song/url',
+		method: 'GET',
+		data,
+	})
+}
+
+// 获取歌曲详情
+export function apiSongDetail(data) {
+	return uni.request({
+		url: baseUrl + '/song/detail',
+		method: 'GET',
+		data,
+	})
+}
---
+++ main.js
@@ -0,0 +1,32 @@
+import App from './App'
+
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+//公共js
+import PubFuc from'./common/js/util.js'
+Vue.prototype.$pubFuc = PubFuc
+
+
+//挂载
+Vue.prototype.$imgSuffix = '?imageView&thumbnail=369x0&quality=75&tostatic=0';
+
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
